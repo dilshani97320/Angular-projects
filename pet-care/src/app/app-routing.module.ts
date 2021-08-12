b9feae4929eb092327a8d0298c2267d4f2fbe1ff
[dilshani] <[dilshanithathsara@gmail.com]>
2021-08-12 23:24:00 +0530

    modified

--- pet-care/src/app/app-routing.module.ts
+++ pet-care/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CatsComponent } from './cats/cats.component';
+import { ContactComponent } from './contact/contact.component';
+import { DogsComponent } from './dogs/dogs.component';
+import { HomeComponent } from './home/home.component';
+import { PetsComponent } from './pets/pets.component';
+import { TypesComponent } from './types/types.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:'',component:HomeComponent},
+ {path:'about',component:AboutComponent},
+ {path:'pets',component:PetsComponent},
+ {path:'types',component:TypesComponent},
+ {path:'dogs',component:DogsComponent},
+ {path:'cats',component:CatsComponent},
+ {path:'contact',component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
