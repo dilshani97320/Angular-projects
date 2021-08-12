2ba626b82951919ce28423cc687c6e2cf4f8cc83
[dilshani] <[dilshanithathsara@gmail.com]>
2021-08-12 23:26:01 +0530

    modified

--- pet-care/src/app/app.module.ts
+++ pet-care/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PetsComponent } from './pets/pets.component';
+import { TypesComponent } from './types/types.component';
+import { CatsComponent } from './cats/cats.component';
+import { DogsComponent } from './dogs/dogs.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PetsComponent,
+    TypesComponent,
+    CatsComponent,
+    DogsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
